6053dcd019b0693f19c08727b12c96bfcaccde58
Rémi Dupuy <remi.dupuy@dsi-globalserives.fr>
2020-12-01 10:51:47 +0100

    rebuild

--- dist/server.js
+++ dist/server.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./config/app");
 const environment_1 = require("./environment");
 const PORT = environment_1.default.getPort();
-app_1.default.listen(PORT, () => {
-    console.log('API REST - AOS GMAO is running on port ' + PORT);
+const port = process.env.PORT || 8080;
+app_1.default.listen(port, () => {
+    console.log('API REST - AOS GMAO is running on port ' + port);
 });
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from "./config/app";
 import env from './environment'
 
 const PORT = env.getPort();
-
-app.listen(PORT, () => {
-   console.log('API REST - AOS GMAO is running on port ' + PORT);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+   console.log('API REST - AOS GMAO is running on port ' + port);
 });
